528fd7266977e51221134990a0387eb332ef5675
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2021-05-22 10:54:11 +0200

    Update client name and add Sequalizer [WIP]

---
+++ commands/general/donate.js
@@ -0,0 +1,37 @@
+/*
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const { donation } = require('../../config.json');
+
+class DonateCommand extends Command {
+    constructor() {
+        super('donate', {
+            aliases: ['donate'],
+            category: 'general',
+            clientPermissions: ["SEND_MESSAGES"],
+            description: {
+                content: 'Send you a donate link to support Paanya',
+                usage: '[]',
+                examples: ['']
+            }
+        });
+    }
+
+    async exec(message) {
+
+        if (!donation.url) return message.channel.send('No donations has been setup for that bot yet');
+
+        const embed = new MessageEmbed()
+            .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+            .setTitle('Donation link')
+            .setURL(donation.url)
+            .setDescription(`Donations are optional, but are appreciated if you want to support ${this.client.user.tag} 💛`);
+
+        return message.channel.send(embed);
+
+    }
+}
+
+module.exports = DonateCommand;
+
+ */
---
+++ dbObjects.js
@@ -0,0 +1,12 @@
+/*
+const Sequelize = require('sequelize');
+const config = require('./config.json');
+//const fs = require('fs');
+
+const sequelize = new Sequelize(config.production);
+
+const Donator = require('./models/donator')(sequelize, Sequelize.DataTypes);
+
+module.exports = { Donator };
+
+ */

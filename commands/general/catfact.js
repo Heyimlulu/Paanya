b79a2457820a9bb0cf69b9cdb5214f1a527b9335
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-07-02 16:04:16 +0200

    Add dog fact

---
+++ commands/general/catfact.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord-akairo');
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
+class CatFactCommand extends Command {
+    constructor() {
+        super('catfact', {
+            aliases: ['catfact'],
+            category: 'general',
+            clientPermissions: ["SEND_MESSAGES"],
+            description: {
+                content: 'Get a random cat facts! 🐱',
+                usage: '',
+                examples: ['']
+            }
+        });
+    }
+
+    async exec(message) {
+
+        fetch(`https://catfact.ninja/fact`).then((response) => {
+            return response.json();
+        }).then((response) => {
+
+            const embed = new MessageEmbed()
+                .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+                .setDescription(response.fact)
+
+            message.channel.send(embed);
+        })
+
+    }
+}
+
+module.exports = CatFactCommand;
---
+++ commands/general/dogfact.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord-akairo');
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
+class DogFactCommand extends Command {
+    constructor() {
+        super('dogfact', {
+            aliases: ['dogfact'],
+            category: 'general',
+            clientPermissions: ["SEND_MESSAGES"],
+            description: {
+                content: 'Get a random dog facts! 🐶',
+                usage: '',
+                examples: ['']
+            }
+        });
+    }
+
+    async exec(message) {
+
+        fetch(`https://dog-api.kinduff.com/api/facts`).then((response) => {
+            return response.json();
+        }).then((response) => {
+
+            const embed = new MessageEmbed()
+                .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+                .setDescription(response.facts)
+
+            message.channel.send(embed);
+        })
+
+    }
+}
+
+module.exports = DogFactCommand;

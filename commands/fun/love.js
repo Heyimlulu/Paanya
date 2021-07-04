ae04ecec11984f42b4c1113c40ee2758d633eee5
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-07-04 19:31:32 +0200

    ❤️ Update love command

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -5,73 +5,79 @@ const Canvas = require('canvas');
 class LoveCommand extends Command {
     constructor() {
         super('love', {
-            aliases: ['love'],
+            aliases: ['love', 'lovecalc'],
             category: 'fun',
             args: [
                 {
-                    id: 'member',
-                    type: 'string',
+                    id: 'firstMember',
+                    type: 'member',
                     prompt: {
-                        start: 'Which user do you want me to verify your compatibility?',
+                        start: 'Which user do you want me to calculate love percentage?',
+                        retry: "It doesn't seem to be a valid user, please try again!"
+                    }
+                },
+                {
+                    id: 'secondMember',
+                    type: 'member',
+                    prompt: {
+                        start: 'Which second user do you want me to calculate love percentage?',
                         retry: "It doesn't seem to be a valid user, please try again!"
                     }
                 }
             ],
             description: {
-                content: 'Tell a user the love he/she has to another person',
-                usage: '[@user]',
-                examples: ['@user']
+                content: 'Calculate the love percentage between two users.',
+                usage: '[@user1 @user2]',
+                examples: ['']
             }
         });
     }
 
     async exec(message, args) {
 
-        // Create a 700x250 pixels canvas and get its context
-        // The context will be used to modify the canvas
-        const canvas = Canvas.createCanvas(475, 250);
+        // Get members
+        let firstMember = args.firstMember;
+        let secondMember = args.secondMember;
+
+        if (!firstMember || !secondMember) return;
+
+        let description;
+        let heart = '💗';
+        
+        let love = Math.floor((Math.random() * 101)); // Generate a random number between 0 and 100
+
+        if (love == 0) {
+            description = 'Not a chance';
+            heart = '💔';
+        } else if (love < 50) {
+            description = 'It\'s low, but don\'t give up';
+            heart = '💔';
+        } else if (love == 50) {
+            description = 'It\'s a fifty fifty';
+        } else if (love > 50) {
+            description = 'You have your chance';
+        } else if (love == 100) {
+            description = 'It\'s a perfect match!';
+        }
+
+        const canvas = Canvas.createCanvas(425, 200);
         const context = canvas.getContext('2d');
 
-        let member = args.member;
-        let description = '';
-        
-        let love = Math.random() * 100; // Generate a random number between 0 and 100
-        let loveIndex = Math.floor(love / 10);
-        //let loveLevel = '💖'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex);
+        const firstAvatar = await Canvas.loadImage(firstMember.user.displayAvatarURL({format: 'jpg'}));
+        const SecondAvatar = await Canvas.loadImage(secondMember.user.displayAvatarURL({format: 'jpg'}));
 
-        if (love < 50) description = 'It\'s low, but don\'t give up';
-        if (love == 50) description = 'It\'s a fifty fifty'
-        if (love > 50) description = 'You have your chance';
-        if (love == 100) description = 'It\'s a perfect match!';
+        context.drawImage(firstAvatar, 0, 0, 200, 200);
+        context.drawImage(SecondAvatar, 225, 0, 200, 200);
 
-        if (message.mentions.users.first()) {
+        const attachment = new MessageAttachment(canvas.toBuffer(), 'couple.png');
 
-            member = message.mentions.members.first();
+        const embed = new MessageEmbed()
+            .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+            .setDescription(`**${firstMember.user.username} + ${secondMember.user.username}** = __${Math.floor(love)}%__ of Love ${heart}\n${description}`)
+            .attachFiles(attachment)
+            .setImage('attachment://couple.png')
 
-            // Wait for Canvas to load the image
-            const authorAvatar = await Canvas.loadImage(message.author.displayAvatarURL({format: 'jpg'}));
-            const memberAvatar = await Canvas.loadImage(member.user.displayAvatarURL({format: 'jpg'}));
-
-            // Move the image downwards vertically and constrain its height to 200, so that it's square
-            context.drawImage(authorAvatar, 25, 25, 200, 200);
-            context.drawImage(memberAvatar, 250, 25, 200, 200);
-
-            // Use the helpful Attachment class structure to process the file for you
-            const attachment = new MessageAttachment(canvas.toBuffer(), 'couple.png');
-
-            const embed = new MessageEmbed()
-                .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
-                .setTitle(`${message.author.username} + ${member.user.username} = 💟 ${Math.floor(love)}%`)
-                .setDescription(description)
-                //.addField('📊', loveLevel)
-                .attachFiles(attachment)
-                .setImage('attachment://couple.png')
-
-            await message.channel.send(embed);
-
-        } else {
-            await message.reply(`You did not mentionned a member!`);
-        }
+        await message.channel.send(embed);
 
     }
 }

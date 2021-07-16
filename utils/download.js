00002254d9db15bf9b6f5cb17dd308165893b3eb
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-07-16 21:34:51 +0200

    Update youtube downloader

--- utils/download.js
+++ utils/download.js
@@ -1,32 +1,23 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const os = require('os');
+const events = require('events');
 
-module.exports = async function(message, url) {
+module.exports = async function(message, url, output) {
 
-    let output = `${os.tmpdir()}/${message.id}_video.mp4`; // filename
+    let eventEmitter = new events.EventEmitter();
 
-    if (url.includes("http") || url.includes("www")) {
+    const video = ytdl(url, { filter: format => format.container === 'mp4' });
 
-        const video = ytdl(url, { filter: format => format.container === 'mp4' });
+    video.pipe(fs.createWriteStream(output));
 
-        video.pipe(fs.createWriteStream(output));
+    video.on('error', function(err) {
+        console.error('error :', err);
+        eventEmitter.emit('error', err.toString());
+    });
 
-        video.on('error', function(err) {
-            console.error('error :', err);
-            message.channel.send("An error has occured, i can't download from the link you provided.")
-        });
+    video.on('end', function() {
+        eventEmitter.emit('end', output);
+    });
 
-        video.on('end', function() {
-            message.channel.send(`Video downloaded by ${message.author.username}`, { files: [output] })
-                .catch(error => {
-                    message.channel.send('Uh Oh... File is too big to be send on discord');
-                    console.error('error :', error);
-                });
-        });
-
-    } else {
-        await message.channel.send("You need to input a valid link!");
-    }
-
+    return eventEmitter;
 }

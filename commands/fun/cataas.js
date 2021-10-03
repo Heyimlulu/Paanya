0e877309f47fee8e67464f006942648523734882
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-10-03 21:58:18 +0200

    Add random cute kitty pics

---
+++ commands/fun/cataas.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+class CataasCommand extends Command {
+    constructor() {
+        super('cataas', {
+            aliases: ['cataas', 'randomcat', 'cats'],
+            category: 'fun',
+            clientPermissions: ['SEND_MESSAGES'],
+            description: {
+                content: 'Get a random cat pics.',
+                usage: '',
+                examples: ['']
+            }
+        });
+    }
+
+    async exec(message) {
+
+        const response = await axios.get('https://cataas.com/cat?json=true');
+
+        const url = response.data.url;
+        const tags = response.data.tags;
+
+        console.log(tags);
+
+        const embed = new MessageEmbed()
+        .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+        .setAuthor(message.member.user.tag, message.member.user.displayAvatarURL())
+        .setImage(`https://cataas.com${url}`)
+        .setFooter(tags)
+
+        await message.channel.send(embed);
+    }
+}
+
+module.exports = CataasCommand;

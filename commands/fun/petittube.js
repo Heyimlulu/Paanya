85316332365fc14371f75b20accdd8e21aee8228
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-10-03 11:51:50 +0200

    Replacing all node-fetch by axios pt.2

--- commands/fun/petittube.js
+++ commands/fun/petittube.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord-akairo');
 const cheerio = require('cheerio');
 const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
 
 class PetitTubeCommand extends Command {
     constructor() {
@@ -20,19 +21,22 @@ class PetitTubeCommand extends Command {
 
         if (!message.channel.nsfw) return message.channel.send('Sorry, you must be in a NSFW channel to fetch a random video!')
 
-        const response = await fetch('https://petittube.com/');
-        const body = await response.text();
+        await axios.get('https://petittube.com/')
+        .then(async (response) => {
 
-        const $ = cheerio.load(body);
-        const url = $('iframe')[0].attribs.src;
+            const body = response.data;
 
-        let embed = new MessageEmbed()
-            .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
-            .setDescription(`Click [here](${url}) to view the video!`)
-            .setThumbnail('https://petittube.com/title.png')
-            .setFooter('Videos might be NSFW so be careful')
+            const $ = cheerio.load(body);
+            const url = $('iframe')[0].attribs.src;
+            
+            let embed = new MessageEmbed()
+                .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
+                .setDescription(`Click [here](${url}) to view the video!`)
+                .setThumbnail('https://petittube.com/title.png')
+                .setFooter('Videos might be NSFW so be careful')
 
-        await message.channel.send(embed);
+            await message.channel.send(embed);
+        });
     }
 }
 
--- commands/fun/shinobu.js
+++ commands/fun/shinobu.js
@@ -1,5 +1,6 @@
 const { Command } = require('discord-akairo');
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
 
 class ShinobuCommand extends Command {
     constructor() {
@@ -17,23 +18,22 @@ class ShinobuCommand extends Command {
 
     async exec(message) {
 
-        if (!message.channel.nsfw) return message.channel.send('You must be in a NSFW channel only to use this command!');
+        // if (!message.channel.nsfw) return message.channel.send('You must be in a NSFW channel only to use this command!');
 
-        fetch('https://waifu.pics/api/sfw/shinobu').then(response => {
-            return response.json();
-        }).then(response => {
+        await axios.get('https://waifu.pics/api/sfw/shinobu')
+        .then(async (response) => {
 
-            const embed = new Discord.MessageEmbed()
+            const result = await response.data;
+
+            const embed = new MessageEmbed()
                 .setColor(message.member ? message.member.displayHexColor : 'RANDOM')
                 .setAuthor(message.member.user.tag, message.member.user.displayAvatarURL())
-                .setImage(response.url)
+                .setImage(result.url)
                 .setFooter('Powered by waifu.pics')
 
             message.channel.send(embed);
 
-
-        })
-
+        });
     }
 }
 
